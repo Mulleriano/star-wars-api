2c0601f78bb2c994ca554528fcc65cad7dab51d3
Igor Muller <igormullersantos@hotmail.com>
2023-06-05 09:54:31 -0300

    animação lottie

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <RouterView />
+
+  <div id="bg">
+    <lottie-player
+      class="back"
+      src="https://assets2.lottiefiles.com/packages/lf20_82kLQImg0d.json"
+      background="transparent"
+      speed="1"
+      loop
+      style="width: 100vw; height: auto"
+      autoplay
+    ></lottie-player>
+  </div>
 </template>
 
 <style scoped>
-
+#bg {
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
 </style>
